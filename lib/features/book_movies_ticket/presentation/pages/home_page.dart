cdc26ec7f90062e4a6fd9f9aba9c4d983480fd5c
piyush-techmetix <103259442+piyushkumar12111111@users.noreply.github.com>
2024-02-04 23:41:33 +0530

    AppbarUpdate

--- lib/features/book_movies_ticket/presentation/pages/home_page.dart
+++ lib/features/book_movies_ticket/presentation/pages/home_page.dart
@@ -2,7 +2,7 @@ import 'package:book_my_movie/features/book_movies_ticket/presentation/movies_cu
 import 'package:book_my_movie/features/book_movies_ticket/presentation/widgets/movies_list_widget.dart';
 import 'package:book_my_movie/src/pages/error_screen.dart';
 import 'package:book_my_movie/src/pages/loading_screen.dart';
-import 'package:book_my_movie/src/widgets/app_bar/custom_appbar.dart';
+//import 'package:book_my_movie/src/widgets/app_bar/custom_appbar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -13,12 +13,12 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     final totalMoviesCount = context.watch<MoviesCubit>().totalMoviesCount;
     return Scaffold(
-      appBar: CustomAppBar(
-        height: MediaQuery.of(context).size.height * 0.1,
-        subTitle: totalMoviesCount != null
-            ? "Mumbai | $totalMoviesCount  Movies"
-            : '',
-      ),
+      // appBar: CustomAppBar(
+      //   height: MediaQuery.of(context).size.height * 0.1,
+      //   subTitle: totalMoviesCount != null
+      //       ? "Mumbai | $totalMoviesCount  Movies"
+      //       : '',
+      // ),
       body: BlocBuilder<MoviesCubit, MoviesState>(
         builder: (context, state) {
           if (state is MoviesLoadingState) {
